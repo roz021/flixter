3713aeec44d80940606d6c4bddb03b23816f140f
Roy Zhang <royzhang@roys-macbook-pro-4.local>
2019-02-12 19:11:57 -0800

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Roy Zhang on 2/12/19.
+//  Copyright © 2019 Roy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
